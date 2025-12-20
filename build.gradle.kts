d409cb3c62af70c483525ba28269a0aacade7acd
rnentjes <info@nentjes.com>
2025-12-20 18:49:44 +0100

    Bump version to `1.3.2` and improve EventSource reconnection handling with a retry mechanism

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "nl.astraeus"
-version = "1.3.0"
+version = "1.3.2"
 
 repositories {
   mavenCentral()
--- src/jsMain/kotlin/nl/astraeus/partials/Main.kt
+++ src/jsMain/kotlin/nl/astraeus/partials/Main.kt
@@ -10,30 +10,36 @@ fun main() {
   window.onload = {
     PartialsHandler.updateHtml(document.body!!)
 
-    val connectionIdInput = document.getElementById(PARTIALS_CONNECTION_ID_HEADER) as? HTMLInputElement
-    if (connectionIdInput != null) {
-      val eventSource = EventSource(
-        "/partials-sse?${PARTIALS_CONNECTION_ID_HEADER}=${connectionIdInput.value}"
-      )
+    connectToEventSource()
+  }
+}
 
-      eventSource.onmessage = { me ->
-        (me.data as? String)?.also {
-          if (it.startsWith("<div>")) {
-            PartialsHandler.handleHtmlResponse(it)
-          }
+private fun connectToEventSource() {
+  val connectionIdInput = document.getElementById(PARTIALS_CONNECTION_ID_HEADER) as? HTMLInputElement
+  if (connectionIdInput != null) {
+    val eventSource = EventSource(
+      "/partials-sse?${PARTIALS_CONNECTION_ID_HEADER}=${connectionIdInput.value}"
+    )
+
+    eventSource.onmessage = { me ->
+      (me.data as? String)?.also {
+        if (it.startsWith("<div>")) {
+          PartialsHandler.handleHtmlResponse(it)
         }
       }
-
-      eventSource.onerror = { me ->
-        console.log("✗ EventSource error:", me)
-        console.log("✗ Ready state:", eventSource.readyState) // 0=CONNECTING, 1=OPEN, 2=CLOSED
-        console.log("✗ Event type:", me.type)
-        eventSource.close();
-      }
-
-      window.addEventListener("beforeunload", { event ->
-        eventSource.close()
-      })
     }
+
+    eventSource.onerror = { me ->
+      console.error("✗ EventSource error:", me)
+      console.error("✗ Ready state:", eventSource.readyState) // 0=CONNECTING, 1=OPEN, 2=CLOSED
+      eventSource.close()
+      window.setTimeout({ connectToEventSource() }, 1000)
+    }
+
+    window.addEventListener("beforeunload", { event ->
+      eventSource.close()
+    })
+  } else {
+    window.setTimeout({ connectToEventSource() }, 1000)
   }
 }
